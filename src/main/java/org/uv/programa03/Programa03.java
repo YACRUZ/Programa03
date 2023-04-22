f09737fa5da1a89e22e21167749d49051c4b013c
YACRUZ <yamatsukino@gmail.com>
2023-04-22 13:51:47 -0600

    create master

---
+++ src/main/java/org/uv/programa03/Programa03.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package org.uv.programa03;
+
+/**
+ *
+ * @author yahir
+ */
+public class Programa03 {
+
+    public static void main(String[] args) {
+
+//        ClaseConcreta c = new ClaseConcreta();
+//        c.imprimir();
+//        
+//        ClaseAHija cah = new ClaseAHija();
+//        cah.imprimir();
+//            ClaseA ca = null;
+//            
+//            ca = new ClaseAHija();
+//            ca.imprimir();
+//            
+//            ca = new ClaseAHija2();
+//            ca.imprimir();
+//            
+//            ca = new ClaseA(){
+//                @Override
+//                public void msg() {
+//                    System.out.println("Otra clase anonima");
+//                }
+//            };
+//            ca.imprimir();
+//        System.out.println("Hello World!");
+
+        IClaseInterface eje = null;
+        eje = new IClaseConcretaUno();
+        eje.msg();
+        
+        eje = new IClaseConcretaDos();
+        eje.msg();
+        
+        eje = new IClaseInterface() {
+            @Override
+            public void msg() {
+                System.out.println("Otro...");
+            }
+        };
+        eje.msg();
+        
+    }
+}
